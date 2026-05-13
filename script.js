dc0a998ac2e0e869641ddfe7e8992278268d6269
clivekiptoo <ckiptoo338@gmail.com>
2026-05-13 18:09:18 +0300

    create contact constructor

---
+++ script.js
@@ -0,0 +1,12 @@
+// ADDRESs BOOK
+
+// Constructor
+function Contact(firstName, lastName, phoneNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+} 
+
+const contact1 = new Contact("Brian", "Otieno", "0712345678")
+
+console.log(contact1);
